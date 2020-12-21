39287456086089e728e830cc51d54f6faab96199
HeyzzWatcharin <blackf.c@hotmail.co.th>
2020-12-21 21:43:22 +0700

    Edit to Hock

--- pages/Index.js
+++ pages/Index.js
@@ -8,51 +8,49 @@ import {
   Image,
 } from 'react-native';
 
-export default class App extends React.Component {
-  state = {
-    email: '',
-    password: '',
-  };
-  render() {
-    return (
-      <View style={styles.container}>
-        <Image source={require('../assets/img/logo.png')} />
-        <Text style={styles.logo}>SHARE</Text>
-        <View style={styles.inputView}>
-          <Text style={styles.TextLabel}>Email</Text>
-          <TextInput
-            style={styles.inputText}
-            onChangeText={(text) => this.setState({email: text})}
-          />
-        </View>
-        <View style={styles.inputView}>
-          <Text style={styles.TextLabel}>Password</Text>
-          <TextInput
-            secureTextEntry
-            style={styles.inputText}
-            onChangeText={(text) => this.setState({password: text})}
-          />
-        </View>
-        <TouchableOpacity style={styles.loginBtn}>
-          <Text style={styles.loginText}>LOGIN</Text>
-        </TouchableOpacity>
+export default () => {
+  return (
+    <View style={styles.Container}>
+      <Image source={require('../assets/img/logo.png')} />
+      <Text style={styles.LoGo}>SHARE</Text>
+      <View style={styles.InputView}>
+        <Text style={styles.TextLabel}>Email</Text>
+        <TextInput
+          style={styles.InputText}
+          onChangeText={(text) => this.setState({email: text})}
+        />
+      </View>
+      <View style={styles.InputView}>
+        <Text style={styles.TextLabel}>Password</Text>
+        <TextInput
+          secureTextEntry
+          style={styles.InputText}
+          onChangeText={(text) => this.setState({password: text})}
+        />
+      </View>
+      <TouchableOpacity style={styles.LoginBtn}>
+        <Text style={styles.LoginText}>LOGIN</Text>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <Text style={styles.CreateAccount}>Create New Account</Text>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <Text style={styles.Continue}>Or Continue With</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.SubLogo}>
         <TouchableOpacity>
-          <Text style={styles.CreateAccount}>Create New Account</Text>
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <Text style={styles.Continue}>Or Continue With</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.SubLogo}>
           <Image
             style={styles.CssFacebook}
             source={require('../assets/img/facebook.png')}
           />
+        </TouchableOpacity>
+        <TouchableOpacity>
           <Image source={require('../assets/img/google.png')} />
         </TouchableOpacity>
-      </View>
-    );
-  }
-}
+      </TouchableOpacity>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   SubLogo: {
@@ -62,27 +60,23 @@ const styles = StyleSheet.create({
   CssFacebook: {
     marginRight: 15,
   },
-  tinyLogo: {
-    width: 50,
-    height: 50,
-  },
   TextLabel: {
     marginBottom: '10%',
     fontSize: 18,
   },
-  container: {
+  Container: {
     flex: 1,
     backgroundColor: '#ffffff',
     alignItems: 'center',
     justifyContent: 'center',
   },
-  logo: {
+  LoGo: {
     fontWeight: 'bold',
     fontSize: 50,
     color: '#3844EE',
     marginBottom: 60,
   },
-  inputView: {
+  InputView: {
     width: '80%',
     backgroundColor: '#6370FF',
     borderColor: 'black',
@@ -92,12 +86,12 @@ const styles = StyleSheet.create({
     padding: 20,
     marginBottom: '10%',
   },
-  inputText: {
+  InputText: {
     height: 50,
     color: 'white',
     fontSize: 22,
   },
-  loginBtn: {
+  LoginBtn: {
     width: '30%',
     backgroundColor: '#6370FF',
     borderRadius: 25,
@@ -106,7 +100,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     marginBottom: 10,
   },
-  loginText: {
+  LoginText: {
     color: 'white',
     fontSize: 22,
   },
