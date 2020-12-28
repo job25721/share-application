ef9540f56a5cdcbc4f4ed165c0e32cf79beea508
job25721 <pathomporn_pankaew@cmu.ac.th>
2020-12-28 23:28:41 +0700

    add dismiss keyboard

--- pages/Login.js
+++ pages/Login.js
@@ -15,77 +15,80 @@ import {Input} from '../components/CustomStyledComponent/Input/CustomInput';
 import {CustomText} from '../components/CustomStyledComponent/Text';
 
 import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome5Pro';
+import {DismissKeyboard} from '../components/DismissKeyboard';
 export default (props) => {
   const {
     navigation: {navigate},
   } = props;
   return (
-    <KeyboardAvoidingView behavior="height">
-      <View style={styles.container}>
-        <View
-          style={{
-            height: '70%',
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}>
-          <Image source={require('../assets/img/logo.png')} />
-          <CustomText color={Colors._indigo_600} spacing={5} type="header">
-            SHARE
-          </CustomText>
-          <View>
-            <CustomText>Email</CustomText>
-            <Input focus rounded width={250} />
-          </View>
-          <View>
-            <CustomText>Password</CustomText>
-            <Input focus rounded width={250} />
-          </View>
-          <Button
-            text="Login"
-            bg={Colors._indigo_500}
-            color={Colors.white}
-            rounded
-            onPress={() => navigate('Home')}
-          />
-        </View>
-        <View
-          style={{
-            alignItems: 'center',
-            justifyContent: 'flex-end',
-          }}>
-          <Button text="Create new Account" color={Colors._indigo_500} />
-          <CustomText color="#b5b5b5" fontSize={15}>
-            Or Continue With
-          </CustomText>
-          <View style={styles.socialLogin}>
+    <DismissKeyboard>
+      <KeyboardAvoidingView behavior="position">
+        <View style={styles.container}>
+          <View
+            style={{
+              height: '75%',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+            <Image source={require('../assets/img/logo.png')} />
+            <CustomText color={Colors._indigo_600} spacing={5} type="header">
+              SHARE
+            </CustomText>
+            <View>
+              <CustomText>Email</CustomText>
+              <Input focus rounded width={250} />
+            </View>
+            <View>
+              <CustomText>Password</CustomText>
+              <Input focus rounded width={250} />
+            </View>
             <Button
-              text={
-                <FontAwesomeIcon
-                  style={[styles.icon, {color: Colors.facebook}]}
-                  name="facebook"
-                  size={30}
-                />
-              }
-              px={0}
-              mx={10}
-              onPress={() => Alert.alert('Facebook')}
-            />
-            <Button
-              text={
-                <FontAwesomeIcon
-                  style={[styles.icon, {color: Colors.google}]}
-                  name="google"
-                  size={30}
-                />
-              }
-              px={0}
-              mx={10}
-              onPress={() => Alert.alert('Google')}
+              text="Login"
+              bg={Colors._indigo_500}
+              color={Colors.white}
+              rounded
+              onPress={() => navigate('Home')}
             />
           </View>
+          <View
+            style={{
+              alignItems: 'center',
+              justifyContent: 'flex-end',
+            }}>
+            <Button text="Create new Account" color={Colors._indigo_500} />
+            <CustomText color="#b5b5b5" fontSize={15}>
+              Or Continue With
+            </CustomText>
+            <View style={styles.socialLogin}>
+              <Button
+                text={
+                  <FontAwesomeIcon
+                    style={[styles.icon, {color: Colors.facebook}]}
+                    name="facebook"
+                    size={30}
+                  />
+                }
+                px={0}
+                mx={10}
+                onPress={() => Alert.alert('Facebook')}
+              />
+              <Button
+                text={
+                  <FontAwesomeIcon
+                    style={[styles.icon, {color: Colors.google}]}
+                    name="google"
+                    size={30}
+                  />
+                }
+                px={0}
+                mx={10}
+                onPress={() => Alert.alert('Google')}
+              />
+            </View>
+          </View>
         </View>
-      </View>
-    </KeyboardAvoidingView>
+      </KeyboardAvoidingView>
+    </DismissKeyboard>
   );
 };
 
