c1df6f0ecc9a55a6155d0aa15dbbebb8c2c8859d
job25721 <pathomporn_pankaew@cmu.ac.th>
2021-01-14 17:08:47 +0700

    merge with ui search

--- App.js
+++ App.js
@@ -1,82 +1,47 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import 'react-native-gesture-handler';
-import Index from './pages/Index';
-import Login from './pages/Login';
-import Detail from './pages/Detail';
-import Home from './pages/Home';
+
+import NewItem from './pages/NewItem';
+
 import {Button} from './components/CustomStyledComponent/Button/CustomButton';
 import FeatherIcon from 'react-native-vector-icons/Feather';
-import Chat from './pages/Chat/Chat';
-import ChatIndex from './pages/Chat/Index';
+import Login from './pages/Login';
 import PersonModal from './pages/Chat/PersonModal';
 import {Colors} from './utils/Colors';
-import Search from './pages/Search';
 
-const MainStack = createStackNavigator();
+import Detail from './pages/Detail';
+import {Alert} from 'react-native';
+import TabScreen from './pages/Index';
+import Chat from './pages/Chat/Chat';
+
 const RootStack = createStackNavigator();
-
-const MainStackScreen = () => {
-  return (
-    <MainStack.Navigator mode="card">
-      <MainStack.Screen
-        name="Search"
-        component={Search}
-        options={{headerShown: false}}
-      />
-      <MainStack.Screen
-        name="People"
-        component={ChatIndex}
-        options={{
-          headerTitle: 'Chat',
-        }}
-      />
-      <MainStack.Screen
-        name="Chat"
-        component={Chat}
-        options={({route}) => ({
-          title: route.params.name,
-        })}
-      />
-      <MainStack.Screen
-        name="Login"
-        component={Login}
-        options={{headerShown: false}}
-      />
-      <MainStack.Screen
-        name="Home"
-        component={Index}
-        options={{headerShown: false}}
-      />
-
-      <MainStack.Screen
-        name="Detail"
-        component={Detail}
-        options={{
-          headerRight: () => (
-            <Button text={<FeatherIcon name="bookmark" size={20} />} />
-          ),
-        }}
-      />
-      <MainStack.Screen
-        name="Index"
-        component={Home}
-        options={{headerShown: false}}
-      />
-    </MainStack.Navigator>
-  );
-};
 
 const RootStackScreen = () => {
   return (
     <NavigationContainer>
       <RootStack.Navigator mode="modal">
         <RootStack.Screen
-          name="Main"
-          component={MainStackScreen}
+          name="Login"
+          component={Login}
           options={{headerShown: false}}
+        />
+        <RootStack.Screen
+          name="Tab"
+          component={TabScreen}
+          options={{headerShown: false}}
+        />
+
+        <RootStack.Screen
+          name="Detail"
+          component={Detail}
+          options={{
+            headerRight: () => (
+              <Button text={<FeatherIcon name="bookmark" size={20} />} />
+            ),
+          }}
         />
         <RootStack.Screen
           name="PersonModal"
