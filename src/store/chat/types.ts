d54a7c60b2946ae5159394c4d2fa2cf44c6e7f1b
job25721 <pathomporn_pankaew@cmu.ac.th>
2021-03-23 04:51:33 +0700

    fix payload newDirect type

--- src/store/chat/types.ts
+++ src/store/chat/types.ts
@@ -61,7 +61,7 @@ export type ChatActionTypes =
   | {type: 'SET_CHAT_WITH'; payload: User | null}
   | {type: 'SET_CURRENT_PROCESS_REQUEST'; payload: Request | null}
   | {type: 'SET_LOADING_ACTION'; payload: boolean}
-  | {type: 'SET_NEW_DIRECT'; payload: NewDirectMessage}
+  | {type: 'SET_NEW_DIRECT'; payload: NewDirectMessage | undefined}
   | {type: 'SET_CHAT_NOTIFICATION'; payload: number}
   | {type: 'ADD_CHAT_NOTIFICATION'}
   | {type: 'SET_REQUEST_NOTIFY'; payload: RequestUpdatedNotify | undefined};
